f90de3839360949930061a9c84520d19c7c9bc39
Brett Mayson <brett@mayson.io>
2025-12-23 08:36:10 -0600

    fix includes

--- addons/archipelago/script_component.hpp
+++ addons/archipelago/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT archipelago
 #define COMPONENT_BEAUTIFIED Archipelago
 
-#include "..\..\main\script_mod.hpp"
-#include "..\..\main\script_macros.hpp"
+#include "..\main\script_mod.hpp"
+#include "..\main\script_macros.hpp"
--- addons/north_takistan/script_component.hpp
+++ addons/north_takistan/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT north_takistan
 #define COMPONENT_BEAUTIFIED North Takistan
 
-#include "..\..\main\script_mod.hpp"
-#include "..\..\main\script_macros.hpp"
+#include "..\main\script_mod.hpp"
+#include "..\main\script_macros.hpp"
