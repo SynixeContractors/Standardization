5309e5a6cd110a81214c0ecf8d3414f1e7955165
Patryk Kaniewski <p7tryk@gmail.com>
2026-07-16 17:49:08 +0200

    remove cro

--- addons/jca/CfgWeapons_Sights.hpp
+++ addons/jca/CfgWeapons_Sights.hpp
@@ -133,16 +133,3 @@ class JCA_optic_MROS_olive_magnifier: JCA_optic_MROS_base_magnifier {
 class JCA_optic_MROS_sand_magnifier: JCA_optic_MROS_base_magnifier {
     displayName = "SIG Sauer Romeo 5T (Sand, Magnifier)";
 };
-
-// CRO -> Monstrum Ghost CRS
-// i can't find exact match but this is close enough
-class JCA_optic_CRO_base;
-class JCA_optic_CRO_black: JCA_optic_CRO_base {
-    displayName = "Monstrum Ghost CRS (Black)";
-};
-class JCA_optic_CRO_olive: JCA_optic_CRO_base {
-    displayName = "Monstrum Ghost CRS (Olive)";
-};
-class JCA_optic_CRO_sand: JCA_optic_CRO_base {
-    displayName = "Monstrum Ghost CRS (Sand)";
-};
