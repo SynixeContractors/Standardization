6cb84f940398f70360a98c28695a68d6255deab9
Patryk Kaniewski <p7tryk@gmail.com>
2026-08-23 21:07:17 +0200

    fix bustard fuel (#14)

--- addons/ws/CfgVehicles.hpp
+++ addons/ws/CfgVehicles.hpp
@@ -1,4 +1,8 @@
 class CfgVehicles {
+    class Helicopter_Base_F;
+    class UAV_02_Base_lxWS: Helicopter_Base_F {
+        fuelCapacity = 30; //AR-2 Darter has 19
+    };
     class B_shield_backpack_lxWS;
     class I_shield_backpack_lxWS: B_shield_backpack_lxWS {
         displayName = "Portable Shield Bag (DAP Green)";
